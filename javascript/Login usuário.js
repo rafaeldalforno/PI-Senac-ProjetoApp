f1dcb6c51b8ebfc986c78bdeec2278b45ed81c65
Bruno De Siqueira Dias <72524891+BrunosDias-Benalia@users.noreply.github.com>
2022-09-09 00:58:09 -0300

    Add files via upload
    Adicionada java schipt das telas de login, correção de bugs de formatação e direcionamento de paginas

---
+++ javascript/Login usuário.js
@@ -0,0 +1,19 @@
+var login = document.getElementById('login').value;
+var senha = document.getElementById('senha').value;
+
+
+
+
+function logar(){
+
+            var login = document.getElementById('login').value;
+            var senha = document.getElementById('senha').value;
+
+            if(login == "admin" && senha == "admin"){
+                alert('Sucesso');
+                location.href = "home.html";
+            }else{
+                alert('Usuario ou senha incorretos');
+            }
+
+        }
